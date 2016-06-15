ad707f3fa4442493657c56bb8b9be5cc2d5a9b7c
compas-tis <benoit.deglane@compas-tis.com>
2016-06-15 15:05:46 +0200

    simplify dispatcher

--- app/router/Dispatcher.js
+++ app/router/Dispatcher.js
@@ -3,39 +3,29 @@
 import Route from "./Route.js";
 
 export default class Dispatcher {
-	/**
-	 * Simple json file containing all ressources routes
-	 *
-	 * @param routes object
-	 */
-	buildRouter(routes) {
-		for (let resource in routes) {
-			this.buildRoute(routes[resource], resource);
-		}
-	}
+    /**
+     * Simple json file containing all ressources routes
+     *
+     * @param routes object
+     */
+    buildRouter(routes) {
+        for (let resource in routes) {
+            this.buildRoute(routes[resource], resource, (route, controller)=> {
+                let appRoute = new Route(controller);
+                appRoute.createRoute(route);
+            });
+        }
+    }
 
-	/**
-	 *
-	 * @param routes array
-	 * @param controller string
-	 */
-	buildRoute(routes, controller) {
-		for (let route in routes) {
-			this.createRoute(routes[route], controller);
-			//((route, controller)=> {
-			//	let appRoute = new Route(controller);
-			//	appRoute.createRoute(route);
-			//})(routes[route], controller);
-		}
-	}
-
-	/**
-	 *
-	 * @param route string
-	 * @param controller string
-	 */
-	createRoute(route, controller) {
-		let appRoute = new Route(controller);
-		appRoute.createRoute(route);
-	}
+    /**
+     *
+     * @param routes string
+     * @param controller string
+     * @param callback function
+     */
+    buildRoute(routes, controller, callback) {
+        for (let route in routes) {
+            callback(routes[route], controller);
+        }
+    }
 }
