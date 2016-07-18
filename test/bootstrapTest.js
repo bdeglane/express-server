1a985b1faedab1fec252054cf59f1e6bc8498c08
compas-tis <benoit.deglane@compas-tis.com>
2016-07-18 15:48:10 +0200

    clean

--- test/bootstrapTest.js
+++ test/bootstrapTest.js
@@ -1,1 +1,34 @@
-require('./controller/test.js').default();
+var path = require('path');
+var fs = require('fs');
+
+var testFolder = path.join(__dirname);
+
+var walkSync = function (dir, filelist) {
+    var files = fs.readdirSync(dir);
+    files.forEach(function (file) {
+        if (file != 'bootstrapTest.js') {
+            if (fs.statSync(path.join(dir, file)).isDirectory()) {
+                filelist = walkSync(path.join(dir, file), filelist);
+            }
+            else {
+                filelist.push('.' + '/' + path.relative(testFolder, dir) + '/' + file);
+            }
+        }
+    });
+    return filelist;
+};
+
+var getFile = function (path) {
+    require(path).default();
+};
+
+var executeTest = function (files) {
+    for (var ii = 0; ii < files.length; ii++) {
+        getFile(files[ii]);
+    }
+};
+
+/**
+ * execute test recursively
+ */
+executeTest(walkSync(testFolder, []));
