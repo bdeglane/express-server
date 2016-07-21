e104818d2fa75c69a6fa72433abe9f014d1f4684
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-07-21 12:18:52 +0200

    clean

--- app/controller/User.js
+++ app/controller/User.js
@@ -4,34 +4,36 @@ import UserModel from "../model/UserModel.js";
 import UserDao from "../dao/UserDao.js";
 
 export default class User extends BaseController {
-    /**
-     *
-     * @param req
-     * @param res
-     */
-    constructor(req, res) {
-        super(req,res,'user');
-        this.dao = new UserDao();
-    }
+	/**
+	 *
+	 * @param req
+	 * @param res
+	 */
+	constructor(req, res) {
+		super(req, res, 'user');
+		this.dao = new UserDao();
+		this.model = new UserModel();
+	}
 
-    getAction() {
-        return new Promise((resolve, reject)=> {
-            resolve(this.dao.getAllUser());
-        });
-    }
+	getAction() {
+		return new Promise((resolve, reject)=> {
+			// instance du modele
+			resolve(this.dao.getAllUser());
+		});
+	}
 
-    postAction() {
-        return new Promise((resolve, reject)=> {
-        });
-    }
+	postAction() {
+		return new Promise((resolve, reject)=> {
+		});
+	}
 
-    putAction() {
-        return new Promise((resolve, reject)=> {
-        });
-    }
+	putAction() {
+		return new Promise((resolve, reject)=> {
+		});
+	}
 
-    deleteAction() {
-        return new Promise((resolve, reject)=> {
-        });
-    }
+	deleteAction() {
+		return new Promise((resolve, reject)=> {
+		});
+	}
 }
